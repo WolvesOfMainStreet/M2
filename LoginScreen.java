493c93cd7d4e839d173ad36aba0bae479461663d
oldark87 <oldark87@gmail.com>
2014-02-05 17:05:19 -0500

    uploaded android files

--- LoginScreen.java
+++ LoginScreen.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class LoginScreen extends Activity {
-
+	MainActivityMatt ob;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +15,9 @@ public class LoginScreen extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_activity_matt, menu);
         return true;
     }
+    
+
 }
